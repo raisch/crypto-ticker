0baad5b259da4c94eb39f29a7ba66c20e5c1b1fd
Rob Raisch <raisch@gmail.com>
2018-02-16 17:18:29 -0500

    improved docs, add jsdoc dep

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 'use strict'
 /* eslint-env node, es6 */
 
+/**
+ * Example runner for Ticker.
+ *
+ * @requires module:lib/source/GDAX
+ *
+ * @module
+ */
+
+/**
+ * Ticker Class.
+ * @type {Ticker}
+ */
 const Ticker = require('./lib/Ticker')
+
+/**
+ * Ticker Source.
+ * @type {Object}
+ */
 const source = require('./lib/source/GDAX')
+
+/**
+ * Ticker Symbol.
+ * @type {String}
+ */
 const symbol = 'BTC-USD'
 
-const opts = {symbol, ...source}
-const ticker = new Ticker(opts)
+/**
+ * Creates and starts a new {@link Ticker} using {symbol, ...source}.
+ * @method
+ */
+function start () {
+  const opts = {symbol, ...source}
+  const ticker = new Ticker(opts)
+  ticker.start()
+}
 
-ticker.start()
+start()
